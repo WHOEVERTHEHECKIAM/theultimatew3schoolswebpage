29a53720a6feebdc2e428e8f0a9a7297c5c1bf1f
Trey <155652227+WHOEVERTHEHECKIAM@users.noreply.github.com>
2024-01-11 12:21:46 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -135,3 +135,16 @@ function openCity(evt, cityName) {
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
 }
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
+
