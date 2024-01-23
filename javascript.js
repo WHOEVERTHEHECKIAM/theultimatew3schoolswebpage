910c5986e9f2eecdfd0be652f935535cd95fede3
Trey <155652227+WHOEVERTHEHECKIAM@users.noreply.github.com>
2024-01-23 11:38:20 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -167,3 +167,13 @@ function myFunction() {
 }
 </script>
 
+
+/* Open */
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+}
+
+/* Close */
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+}
